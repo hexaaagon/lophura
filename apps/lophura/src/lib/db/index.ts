c35d4dd780f3c7ed3981842d2930dd8f9aed3193
Hexagonn <128217934+hexaaagon@users.noreply.github.com>
2024-11-23 16:47:22 +0700

    fix(lophura server): use dotenv in drizzle db

--- apps/lophura/src/lib/db/index.ts
+++ apps/lophura/src/lib/db/index.ts
@@ -1,5 +1,8 @@
 import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
+import { config } from "dotenv";
+
+config({ path: "../../.env" });
 
 export const sqlite = new Database(process.env.DATABASE_URL);
 export const db: BetterSQLite3Database = drizzle(sqlite);
