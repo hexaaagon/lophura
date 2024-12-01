efa5a34601d61877728537c2d3683a2854e51758
HexaaSuki <bagasdwiferdian8989@gmail.com>
2024-12-01 02:00:42 +0000

    feat(ui): add button

--- apps/lophura/src/app/(app)/(home)/home/page.tsx
+++ apps/lophura/src/app/(app)/(home)/home/page.tsx
@@ -1,4 +1,9 @@
 "use client";
+import { Construction, FolderOpen, Plus, Upload } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 import { StoreType } from "@/lib/store";
 import { useStoreState } from "easy-peasy";
 
@@ -7,10 +12,27 @@ export default function Home() {
 
   return (
     <main>
-      <h1 className="mb-4 text-2xl font-bold">Profile</h1>
-      <pre className="my-2 rounded-lg bg-secondary p-4">
-        {JSON.stringify(data, null, 2)}
-      </pre>
+      <div className="grid w-full grid-cols-2 gap-4 sm:grid-cols-3 lg:w-3/4 xl:grid-cols-4 2xl:w-1/2">
+        <Button>
+          <Plus /> Create
+        </Button>
+        <Button
+          variant="outline"
+          className="border-dashed hover:border-muted-foreground"
+        >
+          <Upload />
+          <span className="flex gap-1">
+            Upload
+            <p className="hidden xl:block">or drop</p>
+          </span>
+        </Button>
+        <Button variant="outline" className="hidden sm:flex">
+          <FolderOpen /> New folder
+        </Button>
+        <Button variant="outline" className="hidden xl:flex">
+          <Construction /> temp boi
+        </Button>
+      </div>
     </main>
   );
 }
--- apps/lophura/src/app/(app)/template.tsx
+++ apps/lophura/src/app/(app)/template.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ComponentProps } from "react";
+import { ComponentProps, useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -9,15 +9,19 @@ import {
   SidebarTrigger,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/sidebar";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { AppSidebar } from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { useIsMobile } from "@/lib/hooks/use-mobile";
 
@@ -37,52 +41,66 @@ export const staticRouteInfo: [
 ];
 
 export default function AppTemplate({ children }: ComponentProps<"div">) {
+  const [commandOpen, setCommandOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setCommandOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
   const pathname = usePathname();
   const isMobile = useIsMobile();
 
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset className="px-4">
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
-          <div className="flex items-center gap-2 px-4">
-            {isMobile && (
-              <>
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-              </>
-            )}
-            <Breadcrumb>
-              <BreadcrumbList>
-                {staticRouteInfo.reduce((acc, [name, href]) => {
-                  if (pathname === href) {
-                    acc.push(
-                      <BreadcrumbItem key={href}>
-                        <BreadcrumbPage>{name}</BreadcrumbPage>
-                      </BreadcrumbItem>,
-                    );
-                  } else if (pathname.startsWith(href)) {
-                    acc.push(
-                      <BreadcrumbItem key={href}>
-                        <BreadcrumbLink asChild>
-                          <Link href={href}>{name}</Link>
-                        </BreadcrumbLink>
-                      </BreadcrumbItem>,
-                    );
-                    if (pathname !== href) {
-                      acc.push(
-                        <BreadcrumbSeparator key={`${href}-separator`} />,
-                      );
-                    }
-                  }
-                  return acc;
-                }, [] as React.ReactNode[])}
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset className="px-4">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+            <div className="flex w-full items-center gap-2 px-4">
+              {isMobile && (
+                <>
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator orientation="vertical" className="mr-2 h-4" />
+                </>
+              )}
+
+              <Button
+                variant="outline"
+                className="flex w-full justify-between gap-2 text-sm lg:w-3/4 2xl:w-1/2"
+                onClick={() => setCommandOpen(true)}
+              >
+                <span>Search</span>
+                {!isMobile && (
+                  <kbd className="pointer-events-none flex h-5 items-center gap-1 rounded border bg-muted px-1.5 font-grotesque text-[12px] font-medium">
+                    <span>⌘</span>K
+                  </kbd>
+                )}
+              </Button>
+            </div>
+          </header>
+          <main className="flex flex-1 flex-col gap-4 p-4 pt-1">
+            {children}
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+      <CommandDialog open={commandOpen} onOpenChange={setCommandOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 }
